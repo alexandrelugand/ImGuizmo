40d83d9912c1fdf38a0814f7084a76436d7ef7a4
Alexandre Lugand <alexandre.lugand@gmail.com>
2024-04-07 00:48:04 +0200

    feat: Scripting engine using Mono for Desktop and .Net 8.0 and Blazor for Web Assembly

--- premake5.lua
+++ premake5.lua
@@ -1,49 +1,71 @@
-project "ImGuizmo"
-	kind "StaticLib"
-	language "C++"
-	cppdialect "C++20"
-	staticruntime "off"
-	architecture "x64"
-	targetdir ("%{wks.location}/bin/" .. outputdir)
-	objdir ("%{wks.location}/bin-int/" .. tmpdir)
+local _files =
+{
+	"ImCurveEdit.cpp",
+	"ImCurveEdit.h",
+	"ImGradient.cpp",
+	"ImGradient.h",
+	"ImGuizmo.cpp",
+	"ImGuizmo.h",
+	"ImSequencer.cpp",
+	"ImSequencer.h"
+}
 
-	files
-	{
-		"ImCurveEdit.cpp",
-		"ImCurveEdit.h",
-		"ImGradient.cpp",
-		"ImGradient.h",
-		"ImGuizmo.cpp",
-		"ImGuizmo.h",
-		"ImSequencer.cpp",
-		"ImSequencer.h"
-	}
+local _includedirs =
+{
+	".",
+	"../ImGui"
+}
 
-	includedirs
-	{
-		".",
-		"../ImGui"
-	}
+group "Dependencies"
+	project "ImGuizmo"
+		kind "StaticLib"
+		language "C++"
+		cppdialect "C++20"
+		staticruntime "off"
+		architecture "x64"
+		targetdir ("%{wks.location}/bin/" .. outputdir)
+		objdir ("%{wks.location}/bin-int/" .. tmpdir)
 
-	filter "system:windows"
-		systemversion "latest"
+		files(_files)
+		includedirs(_includedirs)
 
-	filter "configurations:Debug"
-		runtime "Debug"
-		symbols "on"
-		optimize "Off"
+		filter "system:windows"
+			systemversion "latest"
 
-	filter "configurations:Release"
-		runtime "Release"
-		optimize "full"
+		filter "configurations:Debug"
+			runtime "Debug"
+			symbols "on"
+			optimize "Off"
 
-	filter { "action:gmake2" }
-		architecture "x86"
-		makesettings [[
-CC = emcc
-CXX = em++
-AR = emar
-		]]
-		targetextension  ".a"
-		buildoptions { "-Wall -Wformat -s DISABLE_EXCEPTION_CATCHING=1" }
-		linkoptions { "-s DISABLE_EXCEPTION_CATCHING=1 -s USE_SDL=2 -s USE_GLFW=3 -s FULL_ES3=1 -s FORCE_FILESYSTEM=1 -s WASM=1 -s ALLOW_MEMORY_GROWTH=1 -s NO_EXIT_RUNTIME=0 -s ASSERTIONS=1 --no-heap-copy" }
+		filter "configurations:Release"
+			runtime "Release"
+			optimize "full"
+
+group "Browser/Dependencies"
+	project "ImGuizmo.Web"
+		kind "StaticLib"
+		targetname "ImGuizmo"
+		targetextension ".a"
+		language "C++"
+		cppdialect "C++20"
+		staticruntime "off"
+		architecture "x64"
+		targetdir ("%{wks.location}/bin/" .. outputdirweb)
+		objdir ("%{wks.location}/bin-int/" .. tmpdirweb)
+
+		files(_files)
+		includedirs(_includedirs)
+
+		filter "system:windows"
+			systemversion "latest"
+
+		filter "configurations:Debug"
+			runtime "Debug"
+			symbols "on"
+			optimize "Off"
+
+		filter "configurations:Release"
+			runtime "Release"
+			optimize "full"
+
+group ""
