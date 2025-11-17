0cf941620eba382ec83e968b4e0f3d980050f73d
Alexandre LUGAND <alexandrelugand@users.noreply.github.com>
2025-11-17 21:48:02 +0100

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -41,31 +41,4 @@ group "Dependencies"
 			runtime "Release"
 			optimize "full"
 
-group "Browser/Dependencies"
-	project "ImGuizmo.Web"
-		kind "StaticLib"
-		targetname "ImGuizmo"
-		targetextension ".a"
-		language "C++"
-		cppdialect "C++20"
-		staticruntime "off"
-		architecture "x64"
-		targetdir ("%{wks.location}/bin/" .. outputdirweb)
-		objdir ("%{wks.location}/bin-int/" .. tmpdirweb)
-
-		files(_files)
-		includedirs(_includedirs)
-
-		filter "system:windows"
-			systemversion "latest"
-
-		filter "configurations:Debug"
-			runtime "Debug"
-			symbols "on"
-			optimize "Off"
-
-		filter "configurations:Release"
-			runtime "Release"
-			optimize "full"
-
 group ""
